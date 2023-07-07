b0672344aef7549611ef4908420ea55d9ef464a2
Katy249 <petrovanton247@gmail.com>
2023-07-07 22:14:21 +0300

    Add migration

---
+++ TestsCaseApp.Blazor/Data/Migrations/20230707191159_Migration001.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestsCaseApp.Blazor.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestCases",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Number = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Precondition = table.Column<string>(type: "TEXT", nullable: false),
+                    RequiredResult = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestCases", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestCaseResults",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    TestCaseId = table.Column<string>(type: "TEXT", nullable: false),
+                    Success = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Comment = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestCaseResults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TestCaseResults_TestCases_TestCaseId",
+                        column: x => x.TestCaseId,
+                        principalTable: "TestCases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestCaseSteps",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    TestCaseId = table.Column<string>(type: "TEXT", nullable: false),
+                    Number = table.Column<int>(type: "INTEGER", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Completed = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestCaseSteps", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TestCaseSteps_TestCases_TestCaseId",
+                        column: x => x.TestCaseId,
+                        principalTable: "TestCases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestCaseResults_TestCaseId",
+                table: "TestCaseResults",
+                column: "TestCaseId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestCaseSteps_TestCaseId",
+                table: "TestCaseSteps",
+                column: "TestCaseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestCaseResults");
+
+            migrationBuilder.DropTable(
+                name: "TestCaseSteps");
+
+            migrationBuilder.DropTable(
+                name: "TestCases");
+        }
+    }
+}
